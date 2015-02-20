e750f795b93f761a0b02b23e569d073cfe2f8dcd
Alex Dillhoff <orbv16@gmail.com>
2015-02-19 18:49:42 -0600

    Fixed syntax error in shaders. Added working CMakeLists

--- Assignment01/shader.cpp
+++ Assignment01/shader.cpp
@@ -26,7 +26,7 @@ Shader::Shader( const GLchar *vertexPath, const GLchar *fragmentPath ) {
     }
 
     const GLchar *vShaderCode = vertexCode.c_str();
-    const GLchar *fShaderCode = fragment;Code.c_str();
+    const GLchar *fShaderCode = fragmentCode.c_str();
 
     // 2. Compile shaders
     GLuint vertex, fragment;
